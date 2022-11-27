e5645f688f0d6b7da6e254f265908cf47195f311
kobubu <ink3091245@gmail.com>
2022-11-27 23:18:37 +0300

    Add project files.

---
+++ LibraryService/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryService.Models
+{
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Lang { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name = {Name}, Lang = {Lang}";
+        }
+    }
+}
---
+++ LibraryService/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryService.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public string Language { get; set; }
+
+        public int Pages { get; set; }
+
+        public int Year { get; set; }
+        public int AgeLimit { get; set; }
+        public int PublicationDate { get; set; }
+        public Author[] Authors { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id = {Id}, Title = {Title}, Category = {Category}, Language = {Language}, Pages = {Pages}, Year = {Year}, AgeLimit = +{AgeLimit}, PublicationDate = {PublicationDate}" +
+                $"Authors: {string.Join<Author>(", ", Authors)}";
+        }
+    }
+}
